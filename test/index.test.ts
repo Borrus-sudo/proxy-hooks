8b4ffd8c7d338aa9bf852a27895e117d1b30f865
Jinmay Jhaveri <jinmayjhaveri006@gmail.com>
2021-12-17 18:37:52 +0530

    feat(tests): add basic tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,37 @@
+import Spy from "../lib";
 describe("it should pass the tests", () => {
-  it("two equals two", () => {
-    expect(2).toEqual(2)
-  })
-})
+  it("should work properly", () => {
+    expect(2).toEqual(2);
+    const obj = Spy(
+      {
+        name: "Joe",
+        printName(name) {
+          return name;
+        },
+        printObjName() {
+          return this.name;
+        },
+      },
+      {
+        canGet() {
+          return true;
+        },
+        tapGet(target, prop, receiver) {
+          //@ts-ignore
+          return Reflect.get(...arguments);
+        },
+        methodArguments(cachedInfo, args) {
+          args[0] = args[0] + "Wallace";
+        },
+        methodReturn(cachedInfo, returnVal) {
+          if (cachedInfo.propName === "printObjName") {
+            return "Joe Biden Modified";
+          }
+          return returnVal;
+        },
+      },
+    );
+    expect(obj.printName("Jipy")).toBe("JipyWallace");
+    expect(obj.printObjName()).toBe("Joe Biden Modified");
+  });
+});
